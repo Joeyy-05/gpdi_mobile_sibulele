42aa89759becc8fb3d73392d8f07503041b20141
Maharani <maharanisitorus5@gmail.com>
2026-05-05 08:05:04 +0700

    chore(admin): manage pub packages and platform-specific utilities

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "org.delcom.gpdi_mobile_sibulele"
+    namespace = "com.example.gpdi_mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "org.delcom.gpdi_mobile_sibulele"
+        applicationId = "com.example.gpdi_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:gpdi_mobile_sibulele/main.dart';
+import 'package:gpdi_mobile/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
